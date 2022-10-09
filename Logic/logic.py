22e6c0a02b37bcccce87bc07bd092f3244c7df31
midi_cable <bogdan.astronomy@gmail.com>
2022-10-09 23:03:12 +0300

    changed local db path

--- Logic/logic.py
+++ Logic/logic.py
@@ -8,7 +8,7 @@ def add_record_to_db(_name, _grade, _name_entry, _grade_entry):
     _name_entry.delete(0, tkinter.END)
     _grade_entry.delete(0, tkinter.END)
 
-    with sqlite3.connect('../TestScores.db') as db:
+    with sqlite3.connect('../Simple-SQLite-Project/TestScores.db') as db:
         cursor = db.cursor()
 
     cursor.execute("""CREATE TABLE IF NOT EXISTS scores(
