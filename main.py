f1a1c6e7d8c83da233edef2332dd70968078bba6
midi_cable <bogdan.astronomy@gmail.com>
2022-10-09 22:55:36 +0300

    test commit

--- main.py
+++ main.py
@@ -3,15 +3,6 @@ from GUI import gui
 import sqlite3
 
 
-# root = tkinter.Tk()
-# gui.init_gui(root)
-# root.mainloop()
-
-with sqlite3.connect('../TestScores.db') as db:
-    cursor = db.cursor()
-
-cursor.execute("""SELECT *
-                FROM scores""")
-
-for r in cursor.fetchall():
-    print(r)
+root = tkinter.Tk()
+gui.init_gui(root)
+root.mainloop()
