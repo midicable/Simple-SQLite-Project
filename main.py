bea8987f68bb88ff8fae6e72ee895b70cf5d98f4
midi_cable <bogdan.astronomy@gmail.com>
2022-10-09 22:51:14 +0300

    added .db file

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import tkinter
 from GUI import gui
+import sqlite3
 
-root = tkinter.Tk()
-gui.init_gui(root)
-root.mainloop()
+
+# root = tkinter.Tk()
+# gui.init_gui(root)
+# root.mainloop()
+
+with sqlite3.connect('../TestScores.db') as db:
+    cursor = db.cursor()
+
+cursor.execute("""SELECT *
+                FROM scores""")
+
+for r in cursor.fetchall():
+    print(r)
